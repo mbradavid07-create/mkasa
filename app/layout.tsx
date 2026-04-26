dc790e00d9dbf5d73770f98be96efb43d71a437e
M'bra <mbradavid07@gmail.com>
2026-04-26 22:37:41 +0200

    PWA MKasa

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,11 +39,10 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="theme-color" content="#16a34a" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="MKasa" />
-        <meta name="theme-color" content="#1D9E75" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <PWAInstaller />
